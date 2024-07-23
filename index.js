7ac793707204b79031ec0ce438b72c7e361e73b4
jamtechdev <jamtechdev@gmail.com>
2024-07-23 16:50:30 +0530

    ots

--- index.js
+++ index.js
@@ -3,25 +3,6 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 const { IgApiClient } = require("instagram-private-api");
-const fs = require("fs");
-const SESSION_FILE_PATH = "./ig_session.json";
-
-// store session
-const saveSession = async () => {
-  const cookies = await ig.state.serializeCookieJar();
-  fs.writeFileSync(SESSION_FILE_PATH, JSON.stringify(cookies));
-};
-
-// Load session cookies from a file
-const loadSession = async () => {
-  if (fs.existsSync(SESSION_FILE_PATH)) {
-    const cookies = JSON.parse(fs.readFileSync(SESSION_FILE_PATH, "utf8"));
-    await ig.state.deserializeCookieJar(cookies);
-    console.log("Session loaded");
-  } else {
-    console.log("No session file found, proceeding with login");
-  }
-};
 
 // Initialize Supabase client
 const { createClient } = require("@supabase/supabase-js");
@@ -56,56 +37,33 @@ const getToken = async () => {
     return accessToken;
   } catch (error) {
     console.error("Error obtaining access token:", error);
-    throw error;
+    throw error; // Re-throw error to be handled by the caller
   }
 };
 
-const ig = new IgApiClient();
-
-const loginToInstagram = async () => {
-  ig.state.generateDevice("heystak.io");
-  await loadSession();
-
-  try {
-    await ig.account.login("heystakio", "Heystak12!");
-    console.log("Logged into Instagram");
-    await saveSession();
-  } catch (e) {
-    if (e.message.includes("checkpoint_required")) {
-      console.log("Checkpoint error triggered");
-      await ig.challenge.auto(true); // Requesting verification code
-
-      console.log("Challenge URL:", ig.state.checkpoint); // Checkpoint URL
-      const { code } = await promptUserForCode(); // Custom function to get code from user
-      await ig.challenge.sendSecurityCode(code);
-    } else {
-      console.error("Error logging in:", e);
-    }
-  }
-};
-const promptUserForCode = async () => {
-  const readline = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  return new Promise((resolve) => {
-    readline.question("Enter the security code: ", (code) => {
-      readline.close();
-      resolve({ code });
-    });
-  });
-};
-
+// get ads
 const getNewMessages = async () => {
+  const ig = new IgApiClient();
+  ig.state.generateDevice("heystak.io");
+  console.log("IG_USERNAME:", "heystakio");
+  console.log("IG_PASSWORD:", "Heystak12!" ? "Loaded" : "Not Loaded");
+
+  await ig.account.login("heystak.io", "Heystak12!");
+
   const inboxFeed = ig.feed.directInbox();
   const threads = await inboxFeed.items();
 
+  // A set to keep track of processed message IDs
   const processedMessageIds = new Set();
+
+  // Load processed message IDs from storage (this is just an example)
+  // In a real application, you would load this from a database or file
+  // const processedMessageIds = new Set(loadProcessedMessageIdsFromStorage());
 
   threads.forEach((thread) => {
     thread.items.forEach((message) => {
       if (!processedMessageIds.has(message.fbid)) {
+        // Process the new message
         console.log("New message:", message.item_type);
         if (message.item_type === "media_share") {
           const post_id = message?.media_share?.id;
@@ -114,11 +72,10 @@ const getNewMessages = async () => {
           const brand_fullname = message?.media_share?.user?.full_name;
           const caption_text = message?.media_share?.caption?.text;
           const ad_id = message?.media_share?.ad_id;
-          const product_images = message?.media_share?.carousel_media?.map(
+          const product_images = carousel_media.map(
             (item) => item.image_versions2.candidates[0].url
           );
-          const product_link = message?.media_share?.carousel_media[0].link;
-
+          const product_link = carousel_media[0].link;
           console.log(
             post_id,
             brand_logo,
@@ -132,12 +89,16 @@ const getNewMessages = async () => {
           console.log(JSON.stringify(message?.media_share));
         }
 
+        // Add the message ID to the set of processed IDs
         processedMessageIds.add(message.fbid);
+
+        // Save the processed message ID to storage (this is just an example)
+        // In a real application, you would save this to a database or file
+        // saveProcessedMessageIdToStorage(message.item_id);
       }
     });
   });
 };
-
 async function callAnotherApi(userData) {
   try {
     if (!isNaN(userData.lastMessage)) {
@@ -170,7 +131,7 @@ async function callAnotherApi(userData) {
           postData,
           {
             headers: {
-              Authorization: `Bearer ${await getToken()}`,
+              Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjgzNmQzNDQxMWJhYmZjNTM5MDc1ODhhODExYTAwMzFkMDQwNWJmOWRiZWY3NDJlZTY2MWE5NWZkMDVmOWNjODlhZTA0YWYzNzM2ZmEzZjFkIn0.eyJhdWQiOiI4NDUyN2E0NjkxMjY4Y2U3YzlhMmFlOGFhZmQxNTljNiIsImp0aSI6IjgzNmQzNDQxMWJhYmZjNTM5MDc1ODhhODExYTAwMzFkMDQwNWJmOWRiZWY3NDJlZTY2MWE5NWZkMDVmOWNjODlhZTA0YWYzNzM2ZmEzZjFkIiwiaWF0IjoxNzIxMzkwOTI3LCJuYmYiOjE3MjEzOTA5MjcsImV4cCI6MTcyMTM5NDUyNywic3ViIjoiIiwic2NvcGVzIjpbXSwidXNlciI6eyJpZCI6ODc3NTcxOCwiZ3JvdXBfaWQiOm51bGwsInBhcmVudF9pZCI6bnVsbCwiY29udGV4dCI6eyJhY2NsaW0iOiIwIn0sImFyZWEiOiJyZXN0IiwiYXBwX2lkIjpudWxsfX0.XIdZyo8mGBdrYBwyds1f2vJKuvI5wAwBqrcmIlZqmyT6bsWqbzn1UoyVcZ6lrLEiEuaTUzclAXNsy-cefmkyUSR6s1PZ_uTaqTf-0U0y3Z6j015DYnEG5vliwbIL2jh139stVQ8UmtLUacL_KZpJeRx0QbFwz6qKK5R01RCEnQ2f805J2EvnVgw3lqrJfPrcpw7O3IZ6GjGwKbLRA-IZtSM0rN4_aKPcPVTJXylUKpBJvnUbklRnR_67aKMdUjWO7p7uxppRx1I7kRzwmdL7tsCYcpfjQ7PpYXTKBD_bHYn_ycKAnjlazp5nZauvMRBIXI7sP_FApYZRVKKLLwbhAw`,
               "Content-Type": "application/json",
             },
           }
@@ -209,7 +170,7 @@ async function callAnotherApi(userData) {
             postData,
             {
               headers: {
-                Authorization: `Bearer ${await getToken()}`,
+                Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjgzNmQzNDQxMWJhYmZjNTM5MDc1ODhhODExYTAwMzFkMDQwNWJmOWRiZWY3NDJlZTY2MWE5NWZkMDVmOWNjODlhZTA0YWYzNzM2ZmEzZjFkIn0.eyJhdWQiOiI4NDUyN2E0NjkxMjY4Y2U3YzlhMmFlOGFhZmQxNTljNiIsImp0aSI6IjgzNmQzNDQxMWJhYmZjNTM5MDc1ODhhODExYTAwMzFkMDQwNWJmOWRiZWY3NDJlZTY2MWE5NWZkMDVmOWNjODlhZTA0YWYzNzM2ZmEzZjFkIiwiaWF0IjoxNzIxMzkwOTI3LCJuYmYiOjE3MjEzOTA5MjcsImV4cCI6MTcyMTM5NDUyNywic3ViIjoiIiwic2NvcGVzIjpbXSwidXNlciI6eyJpZCI6ODc3NTcxOCwiZ3JvdXBfaWQiOm51bGwsInBhcmVudF9pZCI6bnVsbCwiY29udGV4dCI6eyJhY2NsaW0iOiIwIn0sImFyZWEiOiJyZXN0IiwiYXBwX2lkIjpudWxsfX0.XIdZyo8mGBdrYBwyds1f2vJKuvI5wAwBqrcmIlZqmyT6bsWqbzn1UoyVcZ6lrLEiEuaTUzclAXNsy-cefmkyUSR6s1PZ_uTaqTf-0U0y3Z6j015DYnEG5vliwbIL2jh139stVQ8UmtLUacL_KZpJeRx0QbFwz6qKK5R01RCEnQ2f805J2EvnVgw3lqrJfPrcpw7O3IZ6GjGwKbLRA-IZtSM0rN4_aKPcPVTJXylUKpBJvnUbklRnR_67aKMdUjWO7p7uxppRx1I7kRzwmdL7tsCYcpfjQ7PpYXTKBD_bHYn_ycKAnjlazp5nZauvMRBIXI7sP_FApYZRVKKLLwbhAw`,
                 "Content-Type": "application/json",
               },
             }
@@ -228,6 +189,7 @@ async function callAnotherApi(userData) {
   }
 }
 
+// store data from webhook
 function storeData(data) {
   const username = data[0]?.contact.username;
   const lastMessage = data[0]?.contact.last_message;
@@ -238,10 +200,10 @@ function storeData(data) {
 
 const inComingDetails = [];
 
+// Endpoint to receive incoming messages
 app.post("/webhook/incoming", async (req, res) => {
   try {
     const data = req.body;
-    console.log(JSON.stringify(data));
     console.log(data[0]?.info?.message, data);
     const userData = storeData(data);
     const check = await inComingDetails.find(
@@ -249,6 +211,7 @@ app.post("/webhook/incoming", async (req, res) => {
         obj.contact_id === userData.contact_id &&
         obj.lastMessage === userData.lastMessage
     );
+    // console.log(check, "neet", userData, inComingDetails);
     if (check) {
       return res.sendStatus(200);
     } else {
@@ -256,59 +219,40 @@ app.post("/webhook/incoming", async (req, res) => {
       await getNewMessages();
       // await callAnotherApi(userData);
     }
+    // console.log(userData, "neet");
+    // Call another API with the stored data
 
-    return res.sendStatus(200);
+    return res.sendStatus(200); // Corrected to use sendStatus
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
   }
 });
 
+// Endpoint to send outgoing messages
 app.post("/webhook/outgoing", async (req, res) => {
   const data = req.body;
   console.log(data, "neet");
-  return res.sendStatus(200);
+  return res.sendStatus(200); // Corrected to use sendStatus
 });
 
-const VERIFY_TOKEN = "navneet12";
+app.get("/messaging-webhook", (req, res) => {
+  const VERIFY_TOKEN = "navneet123"; // Replace with your verify token
 
-app.get("/webhook", (req, res) => {
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
-  console.log("GET request received", req.query);
-
   if (mode && token) {
     if (mode === "subscribe" && token === VERIFY_TOKEN) {
       console.log("WEBHOOK_VERIFIED");
+      console.log(challenge);
       res.status(200).send(challenge);
     } else {
-      console.log("Forbidden: Invalid token");
       res.sendStatus(403);
     }
   } else {
-    console.log("Bad Request: Missing mode or token");
     res.sendStatus(400);
-  }
-});
-
-app.post("/webhook", (req, res) => {
-  const body = req.body;
-  console.log("POST request received", JSON.stringify(body, null, 2));
-
-  if (body.object === "instagram") {
-    body.entry.forEach((entry) => {
-      const webhookEvent = entry.messaging[0];
-      console.log("Instagram Event", webhookEvent);
-
-      // Handle the event here
-    });
-
-    res.status(200).send("EVENT_RECEIVED");
-  } else {
-    console.log("Not Found: Object is not Instagram");
-    res.sendStatus(404);
   }
 });
 
@@ -317,12 +261,9 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+// Mock function to send a message to Instagram
 async function sendOutgoingMessage(to, body) {
+  // Replace with your actual implementation to send a message to Instagram
   console.log(`Sending message to ${to}: ${body}`);
   return { to, body };
 }
-
-// Ensure you login once at the start
-(async () => {
-  await loginToInstagram();
-})();
